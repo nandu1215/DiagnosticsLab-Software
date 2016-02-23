5db35ae56b8957dd51937de7a943f157012ed6cb
Divya Abilash <divya@Abilashs-MacBook-Pro.local>
2016-02-23 14:09:05 -0800

    Changes in Creating Test case page . fetching the data on button click and creating a new table dynamically and inserting the clicked data to other table dynamically

---
+++ js/testname.js
@@ -0,0 +1,7 @@
+var test = { "testcase": [{ "testname": "Blood Test" }, 
+	{ "testname": "Urine Test" }, 
+	{ "testname": "White Blood Cells Test" }, 
+	{ "testname": "Culture Test" }, 
+	{ "testname": "1 hour Glucose Test" }, 
+	{ "testname": "3 hours Glucose Test" }] 
+};
